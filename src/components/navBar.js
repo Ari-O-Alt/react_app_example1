e2c3fad02d9adf89d80aae910b0ea7ab3be6928f
Silvia Bogdan <titifursec@outlook.com>
2020-10-11 23:51:17 +0200

    Created 'components' directory, installed react-router

---
+++ src/components/navBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const navBar = () => {
+  return (
+    <React.Fragment>
+      <nav className='navbar'>
+        <div className='navBarContainer'></div>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+export default navBar;
